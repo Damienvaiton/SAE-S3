04571bea3bbed0c4eb18b9d976946ed21f711670
BlancAlexis <97460077+BlancAlexis@users.noreply.github.com>
2022-11-18 11:36:35 +0100

    Update window

--- app/src/main/java/com/example/pageacceuil/GraphPage.java
+++ app/src/main/java/com/example/pageacceuil/GraphPage.java
@@ -3,12 +3,11 @@ package com.example.pageacceuil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -19,36 +18,54 @@ import java.util.ArrayList;
 public class GraphPage extends AppCompatActivity {
 
     private LineChart graph;
+    private Spinner refreshRate;
+    private Spinner valuesNeeded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_page);
+        refreshRate=findViewById(R.id.refreshRate);
+        valuesNeeded=findViewById(R.id.valuesNeeded);
+
         creaGraph();
 
 
-    }
-       void creaGraph(){
+        };
 
-           graph= (LineChart) findViewById(R.id.lineChart);
+
+
+
+    //region Description
+    //void creaSpinner(){
+
+
+
+
+
+       void creaGraph() {
+
+           graph = (LineChart) findViewById(R.id.lineChart);
+           refreshRate = (Spinner) findViewById(R.id.refreshRate);
+           valuesNeeded = (Spinner) findViewById(R.id.valuesNeeded);
 
            ArrayList<Entry> Yvalues = new ArrayList<>();
 
-           Yvalues.add(new Entry(0,(float)5.5));
-           Yvalues.add(new Entry(1,(float)6.4));
-           Yvalues.add(new Entry(2,(float)7.6));
-           Yvalues.add(new Entry(3,(float)8.3));
+           Yvalues.add(new Entry(0, (float) (Math.random() * (40 - 25))));
+           Yvalues.add(new Entry(1, (float) (Math.random() * (40 - 25))));
+           Yvalues.add(new Entry(2, (float) (Math.random() * (40 - 25))));
+           Yvalues.add(new Entry(3, (float) (Math.random() * (40 - 25))));
 
-           LineDataSet set= new LineDataSet(Yvalues,"yo");
-
+           LineDataSet set = new LineDataSet(Yvalues, "yo");
            set.setFillAlpha(110);
-
            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
            dataSets.add(set);
 
-           LineData data =new LineData(dataSets);
+           LineData data = new LineData(dataSets);
 
            graph.setData(data);
+
        }
 
-}
+
+    }
