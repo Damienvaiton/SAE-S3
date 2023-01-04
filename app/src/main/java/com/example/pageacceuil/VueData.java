8bed3b2342d26b1408ccadaf69f29bc4d50f4db4
Damien Vaiton <damientiti01@gmail.com>
2023-01-04 21:08:12 +0100

    1st choix tri

--- app/src/main/java/com/example/pageacceuil/VueData.java
+++ app/src/main/java/com/example/pageacceuil/VueData.java
@@ -1,6 +1,12 @@
 package com.example.pageacceuil;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,13 +17,21 @@ import java.io.Serializable;
 public class VueData extends AppCompatActivity implements Serializable {
 
     private ListData listData;
+    private Button btnTriChoix;
+    private Switch switchDesc;
+    private String ChoixTri = "Choix du Tri";
+    private boolean Desc = false;
 
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vue_data);
+        btnTriChoix = findViewById(R.id.SortChoise);
+        switchDesc = findViewById(R.id.switch1);
+        btnTriChoix.setText(ChoixTri);
 /*
 
 
@@ -37,14 +51,91 @@ public class VueData extends AppCompatActivity implements Serializable {
 
 
 
+        btnTriChoix.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(VueData.this, btnTriChoix);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_tri_choix, popupMenu.getMenu());
+
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+
+                        if (switchDesc.isChecked()) {
+                            Desc = true;
+                        } else {
+                            Desc = false;
+                        }
+                       ChoixTri = (String) item.getTitle();
+
+                        btnTriChoix.setText(ChoixTri);
+                        return true;
+                    }
+
+
+
+                });
+                popupMenu.show();
+            }
+        });
+
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
         recyclerView.setLayoutManager((new LinearLayoutManager((this))));
 
 
+        if (ChoixTri.equals("Co²")){
+            if (Desc) {
+                GraphPage.listData.listsortCO2Desc();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            } else {
+                GraphPage.listData.listsortCO2();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            }
+        }
+        if (ChoixTri.equals("Température")){
+            if (Desc) {
+                GraphPage.listData.listsortTempDesc();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            } else {
+                GraphPage.listData.listsortTemp();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            }
+        }
+        if (ChoixTri.equals("Humidité")){
+            if (Desc) {
+                GraphPage.listData.listsortHumDesc();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            } else {
+                GraphPage.listData.listsortHum();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            }
+        }
+        if (ChoixTri.equals("Luminosité")){
+            if (Desc) {
+                GraphPage.listData.listsortLuxDesc();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            } else {
+                GraphPage.listData.listsortLux();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            }
+        }
+        if (ChoixTri.equals("O²")){
+            if (Desc) {
+                GraphPage.listData.listsortO2Desc();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            } else {
+                GraphPage.listData.listsortO2();
+                recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
+            }
+        }
 
-        GraphPage.listData.listsortDateDesc();
+
+
         recyclerView.setAdapter(new DataAdapter(getApplicationContext(), GraphPage.listData));
     }
 }
