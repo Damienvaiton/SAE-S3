81a622843d83f8a12910cdffb43cadf52d9839aa
Damien Vaiton <damientiti01@gmail.com>
2023-01-08 15:05:43 +0100

    creation des pages admin

--- app/src/main/java/com/example/pageacceuil/connectadmin.java
+++ app/src/main/java/com/example/pageacceuil/connectadmin.java
@@ -1,5 +1,6 @@
 package com.example.pageacceuil;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
@@ -11,13 +12,16 @@ import androidx.appcompat.app.AppCompatActivity;
 public class connectadmin extends AppCompatActivity {
 
     ImageButton butnback;
+    ImageButton butnnext;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connectadmin);
 
         butnback = findViewById(R.id.imageButton3);
+        butnnext = findViewById(R.id.Boutonconnexionadmin);
 
         butnback.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +30,15 @@ public class connectadmin extends AppCompatActivity {
                 back = new Intent(connectadmin.this, MainActivity.class);
 
                 startActivity(back);
+            }
+        });
+        butnnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent next;
+                next = new Intent(connectadmin.this, AdminPage.class);
+
+                startActivity(next);
             }
         });
     }
