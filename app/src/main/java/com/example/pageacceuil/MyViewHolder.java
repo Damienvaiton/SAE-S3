4c26a30dd3da13aefb366b3b6a5070c8e65fb449
BlancAlexis <97460077+BlancAlexis@users.noreply.github.com>
2023-01-23 10:53:59 +0100

    marche

--- app/src/main/java/com/example/pageacceuil/MyViewHolder.java
+++ app/src/main/java/com/example/pageacceuil/MyViewHolder.java
@@ -12,42 +12,18 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MyViewHolder extends RecyclerView.ViewHolder {
 
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    Button myButton;
-    TextView temp,lux,co2,o2,humi,temps;
+    //Button myButton;
+    TextView temp, lux, co2, o2, humi, temps;
 
     public MyViewHolder(@NonNull View itemView) {
         super(itemView);
-        //myButton = itemView.findViewById(R.id.btn);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int position = getAdapterPosition();
-             /*   System.out.println("yo"+position +" ");
-                Pop_up deletePopup = new Pop_up(this.);
-                deletePopup.build("Supprimer l'entrée? " + position);
-                deletePopup.getYesButton().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        DatabaseReference myRef = database.getReference("SAE_S3_BD/ESP32/Mesure/" + position );
-                        myRef.child(choixESP).removeValue(position);
-                        spinner.getAdapter().notify();
-                        //  choixESP=spinner.g
-                        deletePopup.dismiss();
-                    }
-                });
-                deletePopup.getNoButton().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        deletePopup.dismiss();
-                    }
-                });
-                break;
-            }*/
-            }
-        });
-
-        //  public MyViewHolder(Context applicationContext, ListData listData) {
-        // super();
-        //}
+        temp=itemView.findViewById(R.id.temp);
+        lux=itemView.findViewById(R.id.lux);
+        humi=itemView.findViewById(R.id.humi);
+        o2=itemView.findViewById(R.id.o2);
+        co2=itemView.findViewById(R.id.co2);
+        temps=itemView.findViewById(R.id.heure);
     }
 }
+
+
