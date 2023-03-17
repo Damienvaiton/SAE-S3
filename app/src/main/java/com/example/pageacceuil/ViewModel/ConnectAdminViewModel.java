0d12efdee8f27505d9dff1755b59424c6aa541e2
BlancAlexis <alexis.blanc@etu.univ-lyon1.fr>
2023-03-17 17:04:16 +0100

    add ConnectAdmin doc

--- app/src/main/java/com/example/pageacceuil/ViewModel/ConnectAdminViewModel.java
+++ app/src/main/java/com/example/pageacceuil/ViewModel/ConnectAdminViewModel.java
@@ -5,17 +5,31 @@ import androidx.lifecycle.ViewModel;
 import com.example.pageacceuil.Model.FirebaseAccess;
 
 public class ConnectAdminViewModel extends ViewModel {
-
+    /**
+     * Tab of string who contains username in [0] and password in [1]
+     */
     private String[] tabID;
+    /**
+     * Instance of FirebaseAcces
+     */
     private FirebaseAccess acces;
 
 
+    /**
+     * Constructor who initialize and fill tabID by calling 'getAdminLog' of FirebaseAccess
+     */
     public ConnectAdminViewModel() {
         acces = FirebaseAccess.getInstance();
-        // acces.setConnetViewModel(ConnectAdminViewModel.this);
         tabID = acces.getAdminLog();
     }
 
+    /**
+     *
+     * @param username Text in EditText on the view
+     * @param mdp Text in EditText on the view
+     * @return a boolean in connexion is valid or not
+     * Verify if the username and password are the same in database and this wrote by users
+     */
     public boolean Verify(String username, String mdp) {
         System.out.println(tabID[0] + "edd" + tabID[1]);
         if ((tabID[0].equals(username)) && (tabID[1].equals(mdp))) {
