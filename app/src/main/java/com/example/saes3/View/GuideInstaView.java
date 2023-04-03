f163018e99d90283761ee4a8143b30279cd15015
BlancAlexis <alexis.blanc@etu.univ-lyon1.fr>
2023-04-03 14:50:29 +0200

    start

---
+++ app/src/main/java/com/example/saes3/View/GuideInstaView.java
@@ -0,0 +1,16 @@
+package com.example.saes3.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.saes3.R;
+
+public class GuideInstaView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_insta_view);
+    }
+}
